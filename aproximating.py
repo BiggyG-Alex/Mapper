dd7fd63a309297b582070c57dcebf0f36670c094
BiggyG <alexcraft2003@gmail.com>
2019-10-15 15:55:11 -0500

    part 3 of the approximating journey now accumulation and fun functions

--- aproximating.py
+++ aproximating.py
@@ -33,10 +33,35 @@ for sides in range(8, 100, 8):
 # Accumulators
 
 acc = 0
-for val in range(1, 6):
+for val in range(0, 101, 1):
     acc = acc + val
 
 
-print(acc)
+acc = 0
+for even in range(0, 101, 2):
+    acc = acc + even
+    print(acc)
 
+    acc = 0
+    for odd in range(0, 51, 2):
+        acc = acc + odd
+    print(acc)
 
+    acc = 0
+    for odd in range(0, 200, 2):
+        acc = acc + odd
+
+    print(acc/100)
+
+# Compute the sum of the first 100 even numbers
+# Compute the sum of the first 50 odd numbers
+# Compute the average of the first 100 odd numbers
+# Write a function that returns the average of the first N numbers, where
+#   N is a parameter
+# Write a function called factorial that computes the product of the first N
+#   numbers, where N is a parameter
+# Each number in the Fibonacci sequence is the sum of the previous two numbers.
+#   The first two numbers in the sequence are 1 and 1.  Compute the 10th
+#   Fibonacci number.
+# Write a function to compute the Nth Fibonacci number, where N is a parameter.
+#   You may assume that N will be greater than or equal to 3.
