146a3e6267fee3a2d0635a4b0391b8cf2cf9496f
BiggyG <alexcraft2003@gmail.com>
2019-10-24 15:28:06 -0500

    second commit for strings and connects with them :-) i have written row Your boat with Mr.Black

--- Codes and stringy things.py
+++ Codes and stringy things.py
@@ -8,4 +8,20 @@ lastName = "Chan"
 
 print(firstName + " " + lastName)
 
+name = firstName + " " + lastName
+lastFirst = lastName + ", " + firstName
 
+print(lastFirst)
+
+# Repetition
+#   repitition opperator
+
+print("Hip "*2 + "Hooray!")
+
+def rowYourBoat():
+    print("Row, "*3 + "your boat")
+    print("Gently down the stream")
+    print("Merrily"*4)
+    print("Life is but a dream")
+
+rowYourBoat()
