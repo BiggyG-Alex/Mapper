1f7531441299da3d3356b90117836f40e0b5cdc7
BiggyG <alexcraft2003@gmail.com>
2019-10-28 15:46:03 -0500

    mapper definitions first

--- Mapper.py
+++ Mapper.py
@@ -1,4 +1,19 @@
-def lettertoindex(letter):
+def LetterToindex(letter):
     from string import ascii_lowercase
     alphabet = ascii_lowercase + ' '
-    idx =
+    idx = alphabet.find(letter)
+    if idx == -1:      # means that it wasn't in the alphabet
+        print("error:", letter, "is not in the alphabet")
+    return idx
+
+def indexToLetter(idx):
+    from string import ascii_lowercase
+    alphabet = ascii_lowercase
+    letter = ''
+    if idx >= len(alphabet):
+        print("error:", idx, "is too large.")
+    elif idx < 0:
+        print("error:", idx, "is too small.")
+    else:
+        letter = alphabet[idx]
+    return letter
